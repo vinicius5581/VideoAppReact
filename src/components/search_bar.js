ce3fdf75f2929ab20537169fb3d032763995ab84
Zain Abidi <hello@zainabidi.com>
2016-06-26 11:18:50 -0700

    Updated the SearchBar component from a function based to a ES6 Class based. Moving on in the world :)

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,15 +1,44 @@
 /**
  * Forming a Connection to React
  */
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 /**
  * SearchBar Component
  * @return {[type]} [description]
  */
-const SearchBar = () => {
-    return <input />
+
+///////////////////////////////////////////////
+// A Functional Component                    //
+// -- Since it literally is a function. Duh! //
+// One function, some info goes in and some  //
+// JSX comes out. Very simple				 //
+///////////////////////////////////////////////
+// const SearchBar = () => {
+//     return <input />
+// }
+
+///////////////////////////////////////////////////
+// A Class Component                             //
+// -- Used whenever you want a component to have //
+// some type of an internal record keeping.      //
+// Some ability to be aware of itself and what's //
+// happened to it since it's been rendered.      //
+// Some ability to introspect itself.            //
+// A bit more aware of itself and tell others as //
+// to what's going on. Greater Complexity.       //
+// -- ES6 Class                                  //
+// ---- JS Object with Properties and Methods    //
+///////////////////////////////////////////////////
+class SearchBar extends Component {
+    //////////////////////////////////////////////
+    // Each component MUST have a Render Method //
+    // The ability to render itself somehow.    //
+    //////////////////////////////////////////////
+    render() {
+        return <input />;
+    }
 }
 
 /**
