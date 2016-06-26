967fc2b54e446d997963b80270083f72dc249406
Zain Abidi <hello@zainabidi.com>
2016-06-26 10:45:46 -0700

    Importing and Exporting sub-components into the ReactJS VideoApp Application :)

---
+++ src/components/search_bar.js
@@ -0,0 +1,20 @@
+/**
+ * Forming a Connection to React
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/**
+ * SearchBar Component
+ * @return {[type]} [description]
+ */
+const SearchBar = () => {
+    return <input />
+}
+
+/**
+ * Let's export the SearchBar Code to other files.
+ * Since all of the components are siloed from each other
+ * unless we explicitly declare a connection between them.
+ */
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-///////////////////////////////////
-// Forming a Connection to React //
-///////////////////////////////////
+/**
+ * Forming a Connection to React
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/////////////////////////
-// YouTube Data API v3 //
-// -- Browser Key      //
-/////////////////////////
-const API_KEY = 'AIzaSyC7btKSBmhm7nloIrYdiFgkH9TQpgpM7II'; 
+/**
+ * Forming a Connection to sub-components
+ */
+import SearchBar from './components/search_bar';
+
+/**
+ * YouTube Data API v3 - Browser Key
+ * @type {String}
+ */
+const API_KEY = 'AIzaSyC7btKSBmhm7nloIrYdiFgkH9TQpgpM7II';
 
 //////////////////////////////////////////////////////////////
 // Create a new Component.                                  //
@@ -21,19 +26,22 @@ const API_KEY = 'AIzaSyC7btKSBmhm7nloIrYdiFgkH9TQpgpM7II';
 //////////////////////////////////////////////////////////////
 
 /**
- * [Just a Component]
- * @param {[const]} VideoApp [declaring a variable, just like 'var'... however, 'const' means that this is the finalvalue of this variable. It's never going to change. It's a constant. You'll never reassign 'VideoApp' down the line.]
- * @return {[JSX]} [The HTML looking structure in the return is JSX. A subset / dialect of JS which allows us to write what looks like HTMl inside JS but really behind the scenes is JS. Babel + Webpack will transpile this for the browser to read (Vanilla JS).]
- * @param {[=>]} [New way of declaring a function using ES6. Different value of 'this' ]
+ * Just a Component
+ * @param {const} VideoApp declaring a variable, just like 'var'... however, 'const' means that this is the finalvalue of this variable. It's never going to change. It's a constant. You'll never reassign 'VideoApp' down the line.
+ * @param {=>} New way of declaring a function using ES6. Different value of 'this' 
+ * @return {JSX} The HTML looking structure in the return is JSX. A subset / dialect of JS which allows us to write what looks like HTMl inside JS but really behind the scenes is JS. Babel + Webpack will transpile this for the browser to read (Vanilla JS).
  */
 const VideoApp = () => {
-    return <div>I will be playing a couple of videos here :)</div>;
+    return (
+    	<div>
+    		<p>I will be playing a couple of videos here :)</p>
+			<SearchBar />
+		</div>
+	);
 }
 
-//////////////////////////////////////////////
-// Take this Component's generated HTML and //
-// put it on the page (in the DOM)          //
-//////////////////////////////////////////////
-
+/**
+ * Take this Component's generated HTML and put it
+ * on the page (in the DOM).
+ */
 ReactDOM.render(<VideoApp />, document.querySelector('.container'));
- 
