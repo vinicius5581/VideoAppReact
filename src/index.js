72ffc9e8cff23ad49f1f2441196944361d93159b
Zain Abidi <hello@zainabidi.com>
2016-06-26 10:21:33 -0700

    Adding some sub-components, and updating the App name to VideoApp. Also adding in a Youtube API Key :)

--- src/index.js
+++ src/index.js
@@ -4,22 +4,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/////////////////////////
+// YouTube Data API v3 //
+// -- Browser Key      //
+/////////////////////////
+const API_KEY = 'AIzaSyC7btKSBmhm7nloIrYdiFgkH9TQpgpM7II'; 
+
 //////////////////////////////////////////////////////////////
 // Create a new Component.                                  //
 // -- This component should produce some HTML               //
 // ---- A component is a function or an object that returns //
 // some amount of HTML. We can have different components    //
 // for different functions. For diff purposes.              //
+// -- No matter how small, no matter what the purpose       //
+// always remember to have ONLY ONE component per file      //
 //////////////////////////////////////////////////////////////
 
 /**
  * [Just a Component]
- * @param {[const]} Jack [declaring a variable, just like 'var'... however, 'const' means that this is the finalvalue of this variable. It's never going to change. It's a constant. You'll never reassign 'Jack' down the line.]
+ * @param {[const]} VideoApp [declaring a variable, just like 'var'... however, 'const' means that this is the finalvalue of this variable. It's never going to change. It's a constant. You'll never reassign 'VideoApp' down the line.]
  * @return {[JSX]} [The HTML looking structure in the return is JSX. A subset / dialect of JS which allows us to write what looks like HTMl inside JS but really behind the scenes is JS. Babel + Webpack will transpile this for the browser to read (Vanilla JS).]
  * @param {[=>]} [New way of declaring a function using ES6. Different value of 'this' ]
  */
-const Jack = () => {
-    return <div>What's up dude?</div>;
+const VideoApp = () => {
+    return <div>I will be playing a couple of videos here :)</div>;
 }
 
 //////////////////////////////////////////////
@@ -27,4 +35,5 @@ const Jack = () => {
 // put it on the page (in the DOM)          //
 //////////////////////////////////////////////
 
-ReactDOM.render(<Jack />, document.querySelector('.container'));
+ReactDOM.render(<VideoApp />, document.querySelector('.container'));
+ 
